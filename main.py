7f6bc2a705e13362cdf2f10c424fc64ef0ce8962
rootkaus <runsaucework@gmail.com>
2025-04-25 22:11:37 +0200

    Update main.py

--- main.py
+++ main.py
@@ -26,7 +26,7 @@ tokens = {
     "PENGU": ("pudgy-penguins", "@pudgypenguins"),
     "GIGACHAD": ("gigachad-2", "@GIGACHAD_meme"),
     "PONKE": ("ponke", "@ponkesol"),
-    "FITCOIN": ("fitcoin-2", "@itsfitcoin"),
+    "GOAT": ("goatseus-maximus", "@gospelofgoatse"),
 }
 
 INVEST_AMOUNT = 100
